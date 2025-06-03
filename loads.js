b37896c2b361765e8c2d18cac58a8191c02804ff
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-06-03 17:15:17 -0600

    sobre

--- loads.js
+++ loads.js
@@ -2,8 +2,7 @@
 
 // Hacer disponible el array en window para script.js
 window.guests = [
-  { id: "1", name: "Anabella & Leandro", passes: 2 },
-  { id: "2", name: "Macarena & Rogelio",   passes: 2 },
+  { id: "1", name: "Macarena & Rogelio",   passes: 2 },
   // … resto de invitados
 ];
 
